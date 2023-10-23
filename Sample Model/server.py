ba6c17bfbe00291c58b6dd0e9f39b368821a264b
tantjranvua <trinhductan10032001@gmail.com>
2023-10-23 17:34:26 +0700

    test connecting

--- Sample Model/server.py
+++ Sample Model/server.py
@@ -1,15 +1,8 @@
-import socket
-import threading
 import os
 from util import *
 
-MASTER_ADDR = ('172.16.1.136',5555)
-HEADER = 128
-BUFFER_SIZE = 4096
-FORMAT = 'utf-8'
-SEPARATOR = "<SEPARATOR>"
-RECEIVE_SUCCESS = "<RECEIVESUCCESS>"
-MODELFILE = 'test.txt'
+MASTER_ADDR = ('172.16.7.241',5555)
+MODELFILE = 'test.png'
 
 master_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 master_server.bind(MASTER_ADDR)
@@ -30,6 +23,7 @@ while True:
         #     save_file_event.set()
         file_name = MODELFILE
         file_size = os.path.getsize(file_name)
+        print(file_name,file_size)
         send_model(worker_client,file_name,file_size)
         send_config(worker_client)
     except:
@@ -37,10 +31,5 @@ while True:
         worker_client .close()
         print('Close connection!')
         continue
-    # thread = threading.Thread(target=util.handle_client,args=(worker_client,
-    #                                                             addr,
-    #                                                             model,
-    #                                                             patch_list,
-    #                                                             socket_dict,
-    # #                                                             save_file_event))
-    # thread.start()
+    thread = threading.Thread(target=handle_client,args=(worker_client,addr))
+    thread.start()
