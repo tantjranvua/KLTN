b1454b1f00c00d17960996870be193951f9dd1c8
tantjranvua <trinhductan10032001@gmail.com>
2024-01-03 15:24:24 +0700

    done modelbuild

--- Sample Model/modelbuild.py
+++ Sample Model/modelbuild.py
@@ -2,32 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 # Chay bang CPU thay vi GPU
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
+# os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+# Chay tiet kiem ram GPU
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 class buildmodel():
-    def conv_block(self,inputs , filters, block_num,conv_num=2):
-        x = keras.layers.Conv2D(filters,3, activation = 'relu', padding = 'same',name = 'conv_%d_1'%block_num)(inputs)
-        x = keras.layers.Conv2D(filters,3, activation = 'relu', padding = 'same',name = 'conv_%d_2'%block_num)(x)
-        if conv_num==3:
-            x = keras.layers.Conv2D(filters,3, activation = 'relu', padding = 'same',name = 'conv_%d_3'%block_num)(x)
-        x = keras.layers.MaxPooling2D(name = 'max_pooling_%d'%block_num)(x)
-        return x
-
-    def vgg16(self,num_outputs=1000):
-        inputs = keras.layers.Input(shape=(224,224,3),name = 'input')
-        x = self.conv_block(inputs,64,1)
-        x = self.conv_block(x,128,2)
-        x = self.conv_block(x,256,3,3)
-        x = self.conv_block(x,512,4,3)
-        x = self.conv_block(x,512,5,3)
-        x = keras.layers.Flatten()(x)
-        x = keras.layers.Dense(4096,activation = 'relu',name = 'dense_1')(x)
-        x = keras.layers.Dense(4096,activation = 'relu',name = 'dense_2')(x)
-        outputs = keras.layers.Dense(num_outputs,activation = 'softmax',name = 'outputs')(x)
-        model = keras.Model(inputs = inputs, outputs = outputs, name = 'vgg16')
-        return model
-
-    def residual_block(self,inputs, filter,block_num,reduce=False):
+    def residual_block(self, inputs, filter,block_num,reduce=False):
         res = inputs
         x = keras.layers.Conv2D(filter,(3, 3), strides = 2 if reduce else 1,activation = 'relu', padding = 'same',name = f'conv_{block_num}_1')(res)
         x = keras.layers.Conv2D(filter,(3, 3), activation = 'relu', padding = 'same',name = f'conv_{block_num}_2')(x)
@@ -38,8 +17,8 @@ class buildmodel():
         x = keras.layers.Conv2D(filter,(3, 3), activation = 'relu', padding = 'same',name = f'conv_{block_num}_4')(x)
         res = keras.layers.Add(name = f'end_res_bock_{2*block_num}')([x,res])
         return res
-        
-    def resnet18(self,num_outputs=1000):
+    
+    def resnet18(self, num_outputs=1):
         # Inputs
         inputs = keras.layers.Input(shape=(224,224,3),name = 'input')
         x = keras.layers.Conv2D(64,(7, 7),strides = 2, activation = 'relu', padding = 'same',name = 'conv2D')(inputs)
@@ -60,30 +39,25 @@ class buildmodel():
             outputs = keras.layers.Dense(num_outputs,activation = 'softmax',name = 'outputs_softmax')(x)
         model = keras.Model(inputs = inputs, outputs = outputs, name = 'resnet18')
         return model
-    def __init__(self,model_name = 'resnet18',num_outputs=1):
-        if model_name == 'resnet18':
-            self.model = self.resnet18(num_outputs = num_outputs)
-        if model_name == 'vgg16':
-            self.model = self.vgg16(num_outputs=num_outputs)
-        self.opt = keras.optimizers.Adam()
-        self.loss_fn = keras.losses.BinaryCrossentropy()
-        self.metric = keras.metrics.BinaryAccuracy()
-        self.model.compile(optimizer=self.opt, loss= self.loss_fn)
-    
-    def compile_model(self,model):
-        model.compile(optimizer=self.opt, loss= self.loss_fn)
-        return model
-    
+    def __init__(self, optimizer=keras.optimizers.Adam(), loss=keras.losses.BinaryCrossentropy() , metrics=keras.metrics.BinaryAccuracy()):
+        self.model = self.resnet18()
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
+    def compile(self, optimizer=keras.optimizers.Adam(), loss=keras.losses.BinaryCrossentropy() , metrics=keras.metrics.BinaryAccuracy()):
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
+        self.model.compile(optimizer = self.optimizer, loss = self.loss, metrics = self.metrics)
     @tf.function
-    def train_step(self, model, x, y):
+    def train_step(self,x,y):
+        #Can chuyen y ve ma tran hai chieu neu huan luyen voi batch
         with tf.GradientTape() as tape:
-            logits = model(x, training=True)
-            loss_value = self.loss_fn(y, logits)
-        grads = tape.gradient(loss_value, model.trainable_weights)
-        self.metric.update_state(y,logits)
-        return grads        
+            logits = self.model(x, training=True)
+            loss_value = self.loss(y, logits)
+        grads = tape.gradient(loss_value, self.model.trainable_weights)
+        self.metrics.update_state(y, logits)
+        return loss_value,grads
     @tf.function
-    def update_grads(self, model, grads):
-        self.opt.apply_gradients(zip(grads, model.trainable_weights))
-        # print('in hearrrrrrrrrrrrrrrrrrrrrrrrrrrr----------------')
-        # return model
+    def optimize_model(self, grads):
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
